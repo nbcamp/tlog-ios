463b8959bccc057b30f477407fca9515ffc83c0b
jinyongp <dev.jinyongp@gmail.com>
2023-10-13 14:32:35 +0900

    chore: swiftlint 적용

--- TILApp/SceneDelegate.swift
+++ TILApp/SceneDelegate.swift
@@ -3,21 +3,21 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = .init(windowScene: windowScene)
         window?.makeKeyAndVisible()
-        
+
         window?.rootViewController = ViewController()
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {}
+    func sceneDidDisconnect(_: UIScene) {}
 
-    func sceneDidBecomeActive(_ scene: UIScene) {}
+    func sceneDidBecomeActive(_: UIScene) {}
 
-    func sceneWillResignActive(_ scene: UIScene) {}
+    func sceneWillResignActive(_: UIScene) {}
 
-    func sceneWillEnterForeground(_ scene: UIScene) {}
+    func sceneWillEnterForeground(_: UIScene) {}
 
-    func sceneDidEnterBackground(_ scene: UIScene) {}
+    func sceneDidEnterBackground(_: UIScene) {}
 }
