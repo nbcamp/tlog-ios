ad8954322abcf9a0faf2f889e1d9c2e9201e11a5
LeeSeonQ <lsk021181@gmail.com>
2023-10-16 20:47:56 +0900

    feat: 홈 화면 UI 구현

--- TILApp/Views/HomeView/HomeViewController.swift
+++ TILApp/Views/HomeView/HomeViewController.swift
@@ -3,18 +3,49 @@ import PinLayout
 import UIKit
 
 final class HomeViewController: UIViewController {
-    private lazy var label = {
+    let goodJobLabel = {
         let label = UILabel()
-        label.text = "HomeViewController"
+        label.text = "대단해요!! 연속 x일 달성"
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 30)
         label.sizeToFit()
+        label.layer.cornerRadius = 8
         return label
+    }()
+
+    let completedTILLabel = {
+        let label = UILabel()
+        label.text = "오늘의 TIL 작성 완료✅"
+        label.font = UIFont.systemFont(ofSize: 18)
+        label.textAlignment = .center
+        label.sizeToFit()
+        label.layer.cornerRadius = 8
+        return label
+    }()
+
+    let growthImage = {
+        let imageView = UIImageView(image: UIImage(systemName: "pencil"))
+        imageView.tintColor = .systemTeal
+        imageView.layer.borderWidth = 1
+        imageView.layer.cornerRadius = 8
+        imageView.layer.borderColor = UIColor.systemTeal.cgColor
+        return imageView
     }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        setUpUI()
+    }
 
-        view.addSubview(label)
-        label.pin.center()
+    func setUpUI() {
+        navigationItem.hidesBackButton = true
+        view.addSubview(goodJobLabel)
+        view.addSubview(completedTILLabel)
+        view.addSubview(growthImage)
+
+        goodJobLabel.pin.top(15%).hCenter()
+        completedTILLabel.pin.top(21%).hCenter()
+        growthImage.pin.top(30%).bottom(20%).left(7.5%).right(7.5%)
     }
 }
