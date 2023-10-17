5951ad00af1d59e17c9621ee5e4166eae5475617
LeeSeonQ <lsk021181@gmail.com>
2023-10-17 15:51:01 +0900

    홈 화면 UI 작성

--- TILApp/Views/HomeView/HomeViewController.swift
+++ TILApp/Views/HomeView/HomeViewController.swift
@@ -3,9 +3,9 @@ import PinLayout
 import UIKit
 
 final class HomeViewController: UIViewController {
-    var userDidRegisterBlog: Bool = false
-    var TILComplete: Bool = false
-    let goodJobLabel = {
+    private var userDidRegisterBlog: Bool = false // 로그인상태
+    private var TILComplete: Bool = false // TIL 작성 상태
+    private let goodJobLabel = {
         let label = UILabel()
         label.text = "대단해요!! 연속 x일 달성"
         label.textAlignment = .center
@@ -15,9 +15,9 @@ final class HomeViewController: UIViewController {
         return label
     }()
 
-    let completedTILLabel = {
+    private let completedTILLabel = {
         let label = UILabel()
-        label.text = "오늘의 TIL 작성 완료"
+        label.text = "오늘의 TIL 작성 완료  "
         label.font = UIFont.systemFont(ofSize: 18)
         label.textAlignment = .center
         label.sizeToFit()
@@ -25,8 +25,8 @@ final class HomeViewController: UIViewController {
         return label
     }()
 
-    let growthImage = {
-        let imageView = UIImageView(image: UIImage(systemName: "pencil"))
+    private let growthImage = {
+        let imageView = UIImageView(image: UIImage(systemName: "pencil")) // 임시 이미지
         imageView.tintColor = .systemTeal
         imageView.layer.borderWidth = 1
         imageView.layer.cornerRadius = 8
@@ -34,7 +34,7 @@ final class HomeViewController: UIViewController {
         return imageView
     }()
 
-    let hiLabel = {
+    private let hiLabel = {
         let label = UILabel()
         label.text = "ooo님, 안녕하세요!"
         label.textAlignment = .center
@@ -43,7 +43,7 @@ final class HomeViewController: UIViewController {
         return label
     }()
 
-    let registerBlogLabel = {
+    private let registerBlogLabel = {
         let label = UILabel()
         label.text = "블로그를 등록해주세요!"
         label.textAlignment = .center
@@ -52,7 +52,7 @@ final class HomeViewController: UIViewController {
         return label
     }()
 
-    let isCompleteImageView: UIImageView = {
+    private let isCompleteImageView: UIImageView = {
         let imageView = UIImageView()
 //        imageView.layer.borderWidth = 1
 //        imageView.layer.borderColor = UIColor.clear.cgColor
@@ -61,7 +61,7 @@ final class HomeViewController: UIViewController {
         return imageView
     }()
 
-    let registerBlogButton = {
+    private let registerBlogButton = {
         let button = UIButton()
         button.setTitleColor(.white, for: .normal)
         button.setTitle("블로그 등록하기", for: .normal)
@@ -80,7 +80,7 @@ final class HomeViewController: UIViewController {
         configureUI()
     }
 
-    func configureUI() {
+    private func configureUI() {
         if userDidRegisterBlog {
             setUpUIForRegisteredUser()
         } else {
@@ -88,7 +88,7 @@ final class HomeViewController: UIViewController {
         }
     }
 
-    func setUpUIForRegisteredUser() {
+    private func setUpUIForRegisteredUser() {
         navigationItem.hidesBackButton = true
         view.addSubview(goodJobLabel)
         view.addSubview(completedTILLabel)
@@ -96,9 +96,9 @@ final class HomeViewController: UIViewController {
         view.addSubview(isCompleteImageView)
 
         goodJobLabel.pin.top(15%).hCenter()
-        completedTILLabel.pin.top(21%).hCenter(-3%)
+        completedTILLabel.pin.top(21%).hCenter(-5%)
         growthImage.pin.top(30%).bottom(20%).left(7.5%).right(7.5%)
-        isCompleteImageView.pin.top(20.5%).right(25%).after(of: completedTILLabel).height(30)
+        isCompleteImageView.pin.top(20.5%).right(23%).after(of: completedTILLabel).height(30)
 
         if TILComplete == true {
             isCompleteImageView.image = UIImage(systemName: "checkmark.square.fill")
@@ -107,7 +107,7 @@ final class HomeViewController: UIViewController {
         }
     }
 
-    func setUpUIForUnregisteredUser() {
+    private func setUpUIForUnregisteredUser() {
         view.addSubview(hiLabel)
         view.addSubview(registerBlogLabel)
         view.addSubview(registerBlogButton)
@@ -117,7 +117,7 @@ final class HomeViewController: UIViewController {
         registerBlogButton.pin.left(7.5%).right(7.5%).center()
     }
 
-    @objc func registerBlogButtonTapped() {
+    @objc private func registerBlogButtonTapped() {
         print("블로그등록 페이지 뷰전환")
 
 //        let homeViewController = HomeViewController()
