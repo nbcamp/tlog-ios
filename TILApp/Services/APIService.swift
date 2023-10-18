b34ffb079ee29803e775968c657bac6ab37cba4a
jinyongp <dev.jinyongp@gmail.com>
2023-10-18 17:31:04 +0900

    feat: Moya 라이브러리 설치 및 APIService 작성

--- TILApp/Services/APIService.swift
+++ TILApp/Services/APIService.swift
@@ -1,1 +1,35 @@
-enum APIService {}
+import Foundation
+import Moya
+
+enum APIService {
+    case ping
+}
+
+extension APIService: TargetType {
+    var baseURL: URL { .init(string: "http://ec2-13-124-25-166.ap-northeast-2.compute.amazonaws.com:8080")! }
+
+    var path: String {
+        switch self {
+        case .ping:
+            return "/"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .ping:
+            return .get
+        }
+    }
+
+    var task: Moya.Task {
+        switch self {
+        case .ping:
+            return .requestPlain
+        }
+    }
+
+    var headers: [String: String]? {
+        return ["Content-type": "application/json"]
+    }
+}
