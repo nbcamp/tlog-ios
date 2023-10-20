49f6438d2539fd95c4ba845c0258f19880f101cd
Park, Jinyong <dev.jinyongp@gmail.com>
2023-10-20 12:54:58 +0900

    [Feat] Moya 기반 APIService 작성 (#29)
    * feat: Moya 라이브러리 설치 및 APIService 작성
    
    * feat: APIService Auth 요청 작성 및 예시 코드 정리
    
    * feat:  APIService 및 예시 코드 작성
    
    * fix: 자잘한 수정

--- TILApp/Models/AuthUser.swift
+++ TILApp/Models/AuthUser.swift
@@ -1,5 +1,16 @@
 struct AuthUser: Codable {
     let id: Int
     let username: String
-    let profileUrl: String
+    let avatarUrl: String
+}
+
+struct SignInInput: Codable {
+    let username: String
+    let avatarUrl: String?
+    let provider: String
+    let providerId: String
+}
+
+struct SignInOutput: Codable {
+    let accessToken: String
 }
--- TILApp/Services/APIService.swift
+++ TILApp/Services/APIService.swift
@@ -1,1 +1,98 @@
-enum APIService {}
+import Foundation
+import Moya
+
+final class APIService {
+    static let shared: APIService = .init()
+    private init() {}
+
+    private let provider = MoyaProvider<APIRequest>()
+
+    func request(
+        _ target: APIRequest,
+        onSuccess: @escaping (_ response: Response) -> Void,
+        onError: ((_ error: MoyaError) -> Void)? = nil
+    ) {
+        provider.request(target) { result in
+            switch result {
+            case .success(let response):
+                guard let response = try? response.filterSuccessfulStatusAndRedirectCodes() else {
+                    onError?(.statusCode(response)); return
+                }
+                onSuccess(response)
+            case .failure(let error):
+                onError?(error)
+            }
+        }
+    }
+
+    func request<Model: Decodable>(
+        _ target: APIRequest,
+        model: Model.Type,
+        onSuccess: @escaping (_ model: Model) -> Void,
+        onError: ((_ error: MoyaError) -> Void)? = nil
+    ) {
+        provider.request(target) { result in
+            switch result {
+            case .success(let response):
+                guard let response = try? response.filterSuccessfulStatusCodes() else {
+                    onError?(.statusCode(response)); return
+                }
+                guard let model = try? response.map(model) else {
+                    onError?(.jsonMapping(response)); return
+                }
+                onSuccess(model)
+            case .failure(let error):
+                onError?(error)
+            }
+        }
+    }
+}
+
+enum APIRequest {
+    case ping
+    case signIn(SignInInput)
+    case profile
+}
+
+extension APIRequest: TargetType {
+    var baseURL: URL { .init(string: "http://ec2-13-124-25-166.ap-northeast-2.compute.amazonaws.com:8080")! }
+
+    var path: String {
+        switch self {
+        case .ping:
+            return "/"
+        case .signIn(_:):
+            return "/auth/sign-in"
+        case .profile:
+            return "/users/profile"
+        }
+    }
+
+    var method: Moya.Method {
+        switch self {
+        case .ping, .profile:
+            return .get
+        case .signIn(_:):
+            return .post
+        }
+    }
+
+    var task: Moya.Task {
+        switch self {
+        case .ping, .profile:
+            return .requestPlain
+        case .signIn(let payload):
+            return .requestJSONEncodable(payload)
+        }
+    }
+
+    var headers: [String: String]? {
+        var headers: [String: String] = [:]
+        headers.updateValue("application/json", forKey: "Content-type")
+
+        if let accessToken = AuthService.shared.accessToken {
+            headers.updateValue("Bearer \(accessToken)", forKey: "Authorization")
+        }
+        return headers
+    }
+}
