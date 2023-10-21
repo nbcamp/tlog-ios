d07e69a217ee14d5526d61499d55b6933d4ff669
LeeJaeheee <sns3434@gmail.com>
2023-10-21 12:45:59 +0900

    feat: 커뮤니티 목록 화면 구현

---
+++ TILApp/Views/CommunityView/CommunityTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CommunityTableViewCell.swift
+//  TILApp
+//
+//  Created by 이재희 on 10/21/23.
+//
+
+import UIKit
+
+class CommunityTableViewCell: UITableViewCell {
+    let customCommunityTILView = CustomCommunityTILView()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setupView()
+    }
+
+    @available(*, unavailable)
+    required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func setupView() {
+        contentView.addSubview(customCommunityTILView)
+    }
+}
--- TILApp/Views/CommunityView/CommunityViewController.swift
+++ TILApp/Views/CommunityView/CommunityViewController.swift
@@ -1,22 +1,61 @@
 import UIKit
 
 final class CommunityViewController: UIViewController {
-    private lazy var label = {
-        let label = UILabel()
-        label.text = "CommunityViewController"
-        label.sizeToFit()
-        return label
-    }()
+    private let tableView = UITableView()
+
+    let communityData: [[String]] = [
+        ["Blog 1", "팔로워 11", "팔로우", "제목제목", "내용\n내용내용", "2023-10-24"],
+        ["Blog 2", "팔로워 11", "팔로우", "제목제목", "내용\n내용내용", "2023-10-25"],
+        ["Blog 3", "팔로워 11", "팔로우", "제목제목", "내용\n내용내용", "2023-10-26"],
+    ]
+
+    private var searchBar = UISearchBar()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
 
-        view.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-        ])
+        view.addSubview(searchBar)
+
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.register(CommunityTableViewCell.self, forCellReuseIdentifier: "CommunityTableViewCell")
+
+        view.addSubview(tableView)
+    }
+
+    override func viewDidLayoutSubviews() {
+        searchBar.pin.top(view.pin.safeArea).horizontally().height(60)
+        tableView.pin.top(to: searchBar.edge.bottom).horizontally().bottom(view.pin.safeArea)
+    }
+}
+
+extension CommunityViewController: UITableViewDataSource {
+    func tableView(_: UITableView, numberOfRowsInSection _: Int) -> Int {
+        return communityData.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "CommunityTableViewCell", for: indexPath) as? CommunityTableViewCell else {
+            return UITableViewCell()
+        }
+
+        let info = communityData[indexPath.row]
+
+        cell.customCommunityTILView.userView.setup(image: UIImage(), nicknameText: info[0], contentText: info[1], buttonTitle: info[2])
+        cell.customCommunityTILView.TILView.setup(withTitle: info[3], content: info[4], date: info[5])
+        return cell
+    }
+
+    func tableView(_: UITableView, heightForRowAt _: IndexPath) -> CGFloat {
+        return 141
+    }
+}
+
+extension CommunityViewController: UITableViewDelegate {
+    func tableView(_: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedRow = indexPath.row
+
+        // self.navigationController?.pushViewController(blogEditViewController, animated: true)
     }
 }
