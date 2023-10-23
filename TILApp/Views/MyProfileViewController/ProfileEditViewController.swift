7fe257bff256526d70e9e34444384ba6664ecf61
LeeSeonQ <lsk021181@gmail.com>
2023-10-23 16:49:53 +0900

    feat: 사용자 정보 수정 화면 UI 변경

--- TILApp/Views/MyProfileViewController/ProfileEditViewController.swift
+++ TILApp/Views/MyProfileViewController/ProfileEditViewController.swift
@@ -9,29 +9,35 @@ import PinLayout
 import Then
 import UIKit
 
-class ProfileEditViewController: UIViewController {
+final class ProfileEditViewController: UIViewController {
     private lazy var componentView = UIView().then {
         view.addSubview($0)
     }
 
     private lazy var editProfileImageView = UIImageView().then {
-        $0.image = UIImage(named: "")
-        $0.tintColor = .systemTeal
-        $0.layer.borderWidth = 1
+        $0.image = UIImage(systemName: "person.circle.fill")
+        $0.tintColor = UIColor(named: "AccentColor")
+//        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor(named: "AccentColor")?.cgColor
         $0.layer.cornerRadius = 50
-        $0.layer.borderColor = UIColor.systemTeal.cgColor
-        $0.backgroundColor = .systemGray4
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+
+        var tapGesture = UITapGestureRecognizer(target: self, action: #selector(profileImageViewTapped(_:)))
+        $0.isUserInteractionEnabled = true
+        $0.addGestureRecognizer(tapGesture)
     }
 
     private lazy var editProfileButton = UIButton().then {
-        $0.setImage(UIImage(systemName: "person.crop.circle"), for: .normal)
+        $0.setImage(UIImage(systemName: "camera.circle"), for: .normal)
         $0.addTarget(self, action: #selector(editProfileButtonTapped), for: .touchUpInside)
         $0.backgroundColor = .white
     }
 
-    private lazy var nicknameTextFieldView = CustomTextFieldView().then {
+    private lazy var nicknameTextFieldView = CustomTextFieldViewWithValidation().then {
         $0.titleText = "유저 닉네임"
         $0.placeholder = "닉네임을 입력하세요"
+        $0.validationText = ""
     }
 
     override func viewDidLoad() {
@@ -43,7 +49,6 @@ class ProfileEditViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         setUpUI()
-        componentView.flex.layout()
     }
 
     private func setUpUI() {
@@ -51,14 +56,15 @@ class ProfileEditViewController: UIViewController {
             flex.addItem().direction(.row).justifyContent(.center).define { flex in
                 flex.addItem(editProfileImageView).width(100).height(100)
                 flex.addItem(editProfileButton).position(.absolute).size(24).cornerRadius(12)
-                    .bottom(5%).right(37%)
+                    .bottom(8%).right(38%)
             }
             flex.addItem(nicknameTextFieldView).marginTop(10)
         }
         componentView.pin.top(view.pin.safeArea).bottom(50%).left(view.pin.safeArea).right(view.pin.safeArea)
+        componentView.flex.layout()
     }
 
-    @objc func editProfileButtonTapped() {
+    @objc private func editProfileButtonTapped() {
         // TODO: 프로필사진 선택 로직
     }
 
@@ -70,5 +76,17 @@ class ProfileEditViewController: UIViewController {
         // TODO: 이미지,닉네임 저장 로직
         let myProfileViewController = MyProfileViewController()
         present(myProfileViewController, animated: true, completion: nil)
+    }
+
+    private func validateNickname() {
+        // TODO: 닉네임 유효성 검사 로직
+//        let nickname = nicknameTextFieldView.text
+//        if {
+//            print("통과")
+//            nicknameTextFieldView.validationText = "유효한 닉네임입니다."
+//        } else {
+//            print("불통")
+//            nicknameTextFieldView.validationText = "사용 불가능한 닉네임입니다.."
+//        }
     }
 }
