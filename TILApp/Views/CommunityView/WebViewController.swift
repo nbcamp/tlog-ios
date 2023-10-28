ed23bdc2a3eac51c4b3e73434af792d64774334b
LeeSeonQ <139327412+LeeSeonQ@users.noreply.github.com>
2023-10-28 21:02:58 +0900

    [Feat] 웹뷰 페이지 UI 작업 #10 (#49)

---
+++ TILApp/Views/CommunityView/WebViewController.swift
@@ -0,0 +1,99 @@
+import UIKit
+import WebKit
+
+final class WebViewController: UIViewController {
+    // TODO: 눌려진 셀의 post url 받기
+    var postURL: String? {
+        didSet {
+            if let postURL, let url = URL(string: postURL) {
+                let request = URLRequest(url: url)
+                webView.load(request)
+            }
+        }
+    }
+
+    // TODO: false -> 포스트의 하트버튼의 bool 로
+    private var isHeartFilled = false
+
+    private lazy var webView = WKWebView().then {
+        $0.navigationDelegate = self
+        $0.allowsBackForwardNavigationGestures = true
+        view.addSubview($0)
+    }
+
+    private lazy var backButton = UIBarButtonItem(image: UIImage(systemName: "chevron.left"), style: .plain, target: self, action: #selector(backButtonTapped)).then {
+        $0.tintColor = .systemBlue
+    }
+
+    private lazy var forwardButton = UIBarButtonItem(image: UIImage(systemName: "chevron.right"), style: .plain, target: self, action: #selector(forwardButtonTapped)).then {
+        $0.tintColor = .systemBlue
+    }
+
+    private lazy var reloadButton = UIBarButtonItem(image: UIImage(systemName: "arrow.clockwise"), style: .plain, target: self, action: #selector(reloadButtonTapped)).then {
+        $0.tintColor = .systemBlue
+    }
+
+    private lazy var heartButton = UIBarButtonItem(image: UIImage(systemName: "heart"), style: .plain, target: self, action: #selector(heartButtonTapped)).then {
+        $0.tintColor = .accent
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        addBottomToolBar()
+        navigationItem.rightBarButtonItem = heartButton
+//        if let url = URL(string: "https://zeddios.tistory.com/374") {
+//            let request = URLRequest(url: url)
+//            webView.load(request)
+//        }
+//        view.add(webView)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        setUpUI()
+    }
+
+    private func setUpUI() {
+        webView.pin.all(view.pin.safeArea)
+    }
+
+    private func addBottomToolBar() {
+        let flexibleSpaceButtonItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbarItems = [backButton, flexibleSpaceButtonItem, reloadButton, flexibleSpaceButtonItem, forwardButton]
+        navigationController?.isToolbarHidden = false
+    }
+
+    @objc private func backButtonTapped() {
+        webView.goBack()
+    }
+
+    @objc private func forwardButtonTapped() {
+        webView.goForward()
+    }
+
+    @objc private func reloadButtonTapped() {
+        webView.reload()
+    }
+
+    @objc private func heartButtonTapped() {
+        isHeartFilled.toggle()
+        let imageName = isHeartFilled ? "heart.fill" : "heart"
+        heartButton.image = UIImage(systemName: imageName)
+        navigationItem.rightBarButtonItem = heartButton
+        // TODO: 커뮤니티페이지 의 하트버튼 셀이랑 같은값 전달
+    }
+}
+
+extension WebViewController: WKNavigationDelegate {
+    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
+        backButton.isEnabled = webView.canGoBack
+
+        forwardButton.isEnabled = webView.canGoForward
+    }
+}
