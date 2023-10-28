74648a1b03d0970d901dbf1887b6b2b5c8753604
LeeJaeheee <sns3434@gmail.com>
2023-10-28 20:10:24 +0900

    [Feat] 좋아요 아이콘 터치 영역 추가

--- TILApp/Views/Shared/Components/CustomCommunityTILView.swift
+++ TILApp/Views/Shared/Components/CustomCommunityTILView.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 class CustomCommunityTILView: UIView {
+    // TODO: 커뮤니티 페이지 연결 이후 private으로 변경하기
     lazy var userView = CustomUserView()
     lazy var tilView = CustomTILView()
 
@@ -15,18 +16,23 @@ class CustomCommunityTILView: UIView {
     private lazy var heartIcon = UIImageView().then {
         $0.image = UIImage(systemName: "heart")?.withTintColor(.systemGray2, renderingMode: .alwaysOriginal)
         $0.contentMode = .scaleAspectFit
-        $0.frame.size = CGSize(width: 16.5, height: 16.5)
+        $0.pin.width(16.5).height(16.5)
+    }
+
+    private lazy var likeView = UIView().then {
+        $0.pin.width(25).height(25)
     }
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+
         pin.height(161)
 
         addSubview(tilView)
         addSubview(userView)
         addSubview(dateLabel)
         addSubview(heartIcon)
+        addSubview(likeView)
     }
 
     @available(*, unavailable)
@@ -41,7 +47,8 @@ class CustomCommunityTILView: UIView {
         tilView.pin.below(of: userView).horizontally().marginTop(-15)
         dateLabel.pin.left(20).bottom(10)
         heartIcon.pin.right(20).bottom(10)
-        
+        likeView.pin.bottomRight().marginBottom(5).marginRight(15)
+
         tilView.resizeText()
     }
 }
