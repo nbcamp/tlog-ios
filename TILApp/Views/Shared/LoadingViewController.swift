4d55de5e4de234564a7f04745be25330a84a36c0
LeeJaeheee <sns3434@gmail.com>
2023-11-06 12:09:47 +0900

    feat: 앱아이콘 및 런치스크린 추가

--- TILApp/Views/Shared/LoadingViewController.swift
+++ TILApp/Views/Shared/LoadingViewController.swift
@@ -1,6 +1,11 @@
 import UIKit
 
 final class LoadingViewController: UIViewController {
+    private lazy var logoImage = UIImageView(image: UIImage(named: "SignInPageLogo")).then {
+        $0.contentMode = .scaleAspectFill
+        view.addSubview($0)
+    }
+    
     private lazy var loadingView = UIActivityIndicatorView().then {
         $0.startAnimating()
         view.addSubview($0)
@@ -9,6 +14,7 @@ final class LoadingViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         view.backgroundColor = .systemBackground
+        logoImage.pin.all()
         loadingView.pin.center()
     }
 }
