dcbec4886162de0a59be3fcc53c3da05d0c77a66
Park, Jinyong <dev.jinyongp@gmail.com>
2023-11-13 22:42:26 +0900

    [Feat] 블로그 게시물 자동 태그 설정 화면 플레이스홀더 작성 #160 (#163)
    feat: 블로그 게시물 자동 태그 설정 화면 플레이스홀더 작성

--- TILApp/Views/Shared/Components/CustomKeywordView.swift
+++ TILApp/Views/Shared/Components/CustomKeywordView.swift
@@ -1,30 +1,40 @@
 import UIKit
 
 class CustomKeywordView: UIView {
-    
     private var keywords: [KeywordInput] = []
-    
+
     func setKeywords(_ keywords: [KeywordInput], target: Any, tapSelector: Selector, deleteSelector: Selector) {
         self.keywords = keywords
-        self.removeAllSubviews()
-        self.flex.define {
-            for (index, keyword) in keywords.enumerated() {
-                let customTagView = CustomTagView()
-                
-                customTagView.labelText = keyword.keyword
-                customTagView.tags = keyword.tags
-                
-                $0.addItem(customTagView).marginTop(10)
-                customTagView.pin.horizontally()
-                
-                let tapGestureRecognizer = ContextTapGestureRecognizer(target: target, action: tapSelector)
-                tapGestureRecognizer.context["index"] = index
-                
-                customTagView.addGestureRecognizer(tapGestureRecognizer)
-                customTagView.isUserInteractionEnabled = true
-                customTagView.addTargetForButton(target: target, action: deleteSelector, for: .touchUpInside)
+        removeAllSubviews()
+        if keywords.isEmpty {
+            flex.alignItems(.center).define { flex in
+                flex.addItem(UILabel().then {
+                    $0.text = "불러올 게시글의 제목에 포함될 키워드와\n게시물에 적용할 태그를 작성해주세요!"
+                    $0.numberOfLines = 0
+                    $0.font = .boldSystemFont(ofSize: 14)
+                    $0.textColor = .systemGray3
+                }).marginTop(20)
+            }.layout()
+        } else {
+            flex.define {
+                for (index, keyword) in keywords.enumerated() {
+                    let customTagView = CustomTagView()
+
+                    customTagView.labelText = keyword.keyword
+                    customTagView.tags = keyword.tags
+
+                    $0.addItem(customTagView).marginTop(10)
+                    customTagView.pin.horizontally()
+
+                    let tapGestureRecognizer = ContextTapGestureRecognizer(target: target, action: tapSelector)
+                    tapGestureRecognizer.context["index"] = index
+
+                    customTagView.addGestureRecognizer(tapGestureRecognizer)
+                    customTagView.isUserInteractionEnabled = true
+                    customTagView.addTargetForButton(target: target, action: deleteSelector, for: .touchUpInside)
+                }
             }
+            flex.layout(mode: .adjustHeight)
         }
-        self.flex.layout(mode: .adjustHeight)
     }
 }
