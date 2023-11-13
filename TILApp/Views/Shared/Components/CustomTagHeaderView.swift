f58da205106e159fb18a00ce386ba31fb419e952
jinyongp <dev.jinyongp@gmail.com>
2023-11-13 21:50:42 +0900

    feat: 블로그 게시물 자동 태그 설정 화면 플레이스홀더 작성

--- TILApp/Views/Shared/Components/CustomTagHeaderView.swift
+++ TILApp/Views/Shared/Components/CustomTagHeaderView.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 class CustomTagHeaderView: UIView {
-
     private let label = CustomTitleLabel().then {
         $0.text = "블로그 게시물 자동 태그 설정"
         $0.sizeToFit()
@@ -21,7 +20,7 @@ class CustomTagHeaderView: UIView {
         addSubview(label)
         addSubview(line)
         addSubview(button)
-        
+
         pin.height(30)
     }
 
@@ -29,7 +28,7 @@ class CustomTagHeaderView: UIView {
     required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     override func layoutSubviews() {
         super.layoutSubviews()
 
