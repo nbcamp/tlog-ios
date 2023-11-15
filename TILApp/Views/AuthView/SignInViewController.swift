466906eabfd953c4152d47c1cf1cdbfd3b4d2114
LeeJaeheee <sns3434@gmail.com>
2023-11-15 19:45:54 +0900

    fix: 로그인 화면 UI 개선
    - 로고 위치 변경
    - 체크박스로 이용약관 및 개인정보 처리방침 동의해야 로그인 가능하도록 변경

--- TILApp/Views/AuthView/SignInViewController.swift
+++ TILApp/Views/AuthView/SignInViewController.swift
@@ -6,18 +6,23 @@ final class SignInViewController: UIViewController {
     private let authorizationButton = ASAuthorizationAppleIDButton(type: .signIn, style: .white)
 
     private let label = UILabel().then {
-        $0.text = "간편하게 로그인하고 다양한 서비스를 이용해보세요"
-        $0.font = .systemFont(ofSize: 15, weight: .semibold)
+        $0.text = "오늘을 기록하는 습관, 티로깅 여정을 시작하세요!"
+        $0.font = .systemFont(ofSize: 15.5, weight: .bold)
         $0.textColor = .white
         $0.sizeToFit()
     }
 
-    private let privacyLabel = UILabel().then {
-        $0.text = "회원가입 시 이용 약관 및 개인정보 처리방침에 동의합니다 >"
-        $0.font = .systemFont(ofSize: 12, weight: .regular)
-        $0.textColor = .white
-        $0.sizeToFit()
+    private let checkbox = UIButton().then {
+        $0.setImage(UIImage(named: "checkbox.unchecked"), for: .normal)
+        $0.setImage(UIImage(named: "checkbox.checked"), for: .selected)
+        $0.pin.width(16).height(16)
     }
+
+    private lazy var termsLabel = createCustomLabel(text: "이용 약관", isUnderlined: true)
+    private lazy var andLabel = createCustomLabel(text: " 및 ")
+    private lazy var privacyPolicyLabel = createCustomLabel(text: "개인정보 처리방침", isUnderlined: true)
+    private lazy var agreementLabel = createCustomLabel(text: "에 동의합니다.")
+    private let checkboxContainerView = UIView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,13 +33,27 @@ final class SignInViewController: UIViewController {
         authorizationButton.cornerRadius = 50
         authorizationButton
             .addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+        authorizationButton.isEnabled = false
         view.addSubview(authorizationButton)
         view.addSubview(label)
-        view.addSubview(privacyLabel)
-        
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(privacyLabelTapped))
-        privacyLabel.isUserInteractionEnabled = true
-        privacyLabel.addGestureRecognizer(tapGestureRecognizer)
+
+        view.addSubview(checkboxContainerView)
+        checkboxContainerView.flex.direction(.row).alignItems(.center).justifyContent(.center).define { flex in
+            flex.addItem(checkbox).width(16).height(16)
+            flex.addItem(termsLabel).marginLeft(5)
+            flex.addItem(andLabel)
+            flex.addItem(privacyPolicyLabel)
+            flex.addItem(agreementLabel)
+        }
+        checkboxContainerView.pin.height(20)
+
+        let tapGestureTerms = UITapGestureRecognizer(target: self, action: #selector(openTerms))
+        termsLabel.addGestureRecognizer(tapGestureTerms)
+
+        let tapGesturePrivacy = UITapGestureRecognizer(target: self, action: #selector(openPrivacyPolicy))
+        privacyPolicyLabel.addGestureRecognizer(tapGesturePrivacy)
+
+        checkbox.addTarget(self, action: #selector(checkboxTapped), for: .touchUpInside)
     }
 
     override func viewDidLayoutSubviews() {
@@ -42,9 +61,11 @@ final class SignInViewController: UIViewController {
 
         view.pin.all()
         logoImage.pin.all()
-        authorizationButton.pin.hCenter().bottom(12%).width(90%).height(48)
-        label.pin.hCenter().bottom(to: authorizationButton.edge.top).marginBottom(17)
-        privacyLabel.pin.hCenter().top(to: authorizationButton.edge.bottom).marginTop(8)
+        authorizationButton.pin.hCenter().bottom(17%).width(85%).height(48)
+        label.pin.hCenter().top(53.5%).marginBottom(50)
+        checkboxContainerView.pin.horizontally().bottom(to: authorizationButton.edge.top).marginBottom(10)
+
+        checkboxContainerView.flex.layout()
     }
 
     @objc private func handleAuthorizationAppleIDButtonPress() {
@@ -57,12 +78,45 @@ final class SignInViewController: UIViewController {
         authorizationController.presentationContextProvider = self
         authorizationController.performRequests()
     }
-    
-    @objc private func privacyLabelTapped() {
+
+    @objc func checkboxTapped() {
+        checkbox.isSelected.toggle()
+        authorizationButton.isEnabled = checkbox.isSelected
+    }
+
+    @objc private func openTerms() {
         let webViewController = WebViewController()
         webViewController.url = "https://plucky-fang-eae.notion.site/e951a2d004ac4bbdbee73ee6b8ea4d08"
         webViewController.hidesBottomBarWhenPushed = true
         present(webViewController, animated: true)
+    }
+
+    @objc private func openPrivacyPolicy() {
+        let webViewController = WebViewController()
+        webViewController.url = "https:plip.kr/pcc/96e3cd8c-700d-46a1-b007-37443c721874/privacy-policy"
+        webViewController.hidesBottomBarWhenPushed = true
+        present(webViewController, animated: true)
+    }
+
+    private func createCustomLabel(text: String, isUnderlined: Bool = false) -> UILabel {
+        return UILabel().then {
+            if isUnderlined {
+                var attributes: [NSAttributedString.Key: Any] = [
+                    .font: UIFont.boldSystemFont(ofSize: 14),
+                    .foregroundColor: UIColor.white,
+                    .underlineStyle: NSUnderlineStyle.single.rawValue
+                ]
+                let attributedString = NSAttributedString(string: text, attributes: attributes)
+                $0.attributedText = attributedString
+            } else {
+                $0.text = text
+                $0.font = UIFont.systemFont(ofSize: 14)
+                $0.textColor = .white
+            }
+
+            $0.isUserInteractionEnabled = isUnderlined
+            $0.sizeToFit()
+        }
     }
 }
 
@@ -97,7 +151,12 @@ extension SignInViewController: ASAuthorizationControllerDelegate {
     }
 
     func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
-        // TODO: 로그인 실패 후 동작    
+        // TODO: 로그인 실패 후 동작
+        if let error = error as? ASAuthorizationError, error.code == .canceled {
+            DispatchQueue.main.async {
+                self.checkbox.isSelected = true
+            }
+        }
     }
 }
 
