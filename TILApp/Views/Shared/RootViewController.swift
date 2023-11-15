5fb63fe4c8a06c9a7e705184879453debf2b1249
jinyongp <dev.jinyongp@gmail.com>
2023-11-16 00:57:21 +0900

    fix: 탭바가 붕 뜨는 문제 수정

--- TILApp/Views/Shared/RootViewController.swift
+++ TILApp/Views/Shared/RootViewController.swift
@@ -19,14 +19,17 @@ final class RootViewController: UINavigationController {
             .init(title: "내 정보", icon: "person", controller: MyProfileViewController()),
         ]
 
+        setNavigationBarHidden(true, animated: true)
         let tabBarController = UITabBarController().then {
             $0.setViewControllers(tabs.enumerated().map { index, tab in
+                let navigationController = UINavigationController()
                 tab.controller.tabBarItem = .init(
                     title: tab.title,
                     image: .init(systemName: tab.icon),
                     tag: index
                 )
-                return tab.controller
+                navigationController.setViewControllers([tab.controller], animated: false)
+                return navigationController
             }, animated: false)
         }
 
